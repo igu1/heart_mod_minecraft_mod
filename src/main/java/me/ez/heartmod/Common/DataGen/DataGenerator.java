9af3967db8e569265d82dd7d588be169651d1e70
GAMER'S VALLEY <eesaard@gmail.com>
2022-05-29 12:35:44 +0530

    Added DataGenerator.java

---
+++ src/main/java/me/ez/heartmod/Common/DataGen/DataGenerator.java
@@ -0,0 +1,19 @@
+package me.ez.heartmod.Common.DataGen;
+
+import me.ez.heartmod.Main;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerator {
+    @SubscribeEvent
+    public static void onGatheringData(final GatherDataEvent e){
+        net.minecraft.data.DataGenerator dataGenerator = e.getGenerator();
+        ExistingFileHelper helper = e.getExistingFileHelper();
+
+        //Providers
+        //TODO
+    }
+}
